4a893876af22137d6eff7130ccf04984a9f6a7c2
baha-udin <mbaha713m@gmail.com>
2023-01-14 16:28:27 +0700

    add react native vector icons

---
+++ Src/Screen/App/Favourites/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import styles from './styles';
+
+const Favourites = () => {
+  return (
+    <View>
+      <Text>Favourites</Text>
+    </View>
+  );
+};
+
+export default Favourites;
